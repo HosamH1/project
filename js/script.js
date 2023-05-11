9f3ace5434ae2795debd9abbc204dbc610f55ad5
Hosam H <harooubi@gmail.com>
2023-05-11 12:10:39 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function fetchJoke() {
+    fetch('https://api.chucknorris.io/jokes/random')
+        .then(response => response.json())
+        .then(jokeData => {
+            const jokeElement = document.getElementById('joke');
+            jokeElement.textContent = jokeData.value;
+        })
+        .catch(error => {
+            console.log('Error:', error);
+        });
+}
+
+// Event listener for the "Get a New Joke" button click
+const newJokeBtn = document.getElementById('newJokeBtn');
+newJokeBtn.addEventListener('click', fetchJ
